600bc0f14459e83660ffa692276b60a8ea40bd4c
cgarcia2097 <charles.garcia@ryerson.ca>
2020-09-16 03:18:15 -0400

    First upload

---
+++ firmware/spotiPad.ino
@@ -0,0 +1,87 @@
+/**
+ * File: spotiPad.ino
+ * Date: Sept. 15, 2020
+ * Author: @The_Gambler#1212
+ * Description: Provides a button interface and message stack to the SpotiPad
+ * 
+ */
+
+
+#include <Arduino.h>
+#include <Bounce2.h>
+
+#define NUM_KEYS 8
+#define NUM_COMMANDS 8
+
+// Define the button pins
+#define KEY_PIN_1 21
+#define KEY_PIN_2 20
+#define KEY_PIN_3 19
+#define KEY_PIN_4 18
+#define KEY_PIN_5 15
+#define KEY_PIN_6 14
+#define KEY_PIN_7 16
+#define KEY_PIN_8 10
+
+// Define messages
+#define KEY_MSG_1 "<VOL_UP>"
+#define KEY_MSG_2 "<VOL_DOWN>"
+#define KEY_MSG_3 "<PLAY>"
+#define KEY_MSG_4 "<PAUSE>"
+#define KEY_MSG_5 "<PREV>"
+#define KEY_MSG_6 "<NEXT>"
+#define KEY_MSG_7 "<SHUFFLE>"
+#define KEY_MSG_8 "<SEEK_FRONT>"
+
+// Array optimizations
+byte buttonArray[NUM_KEYS] = {
+  KEY_PIN_1 ,
+  KEY_PIN_2 ,
+  KEY_PIN_3 ,
+  KEY_PIN_4 ,
+  KEY_PIN_5 ,
+  KEY_PIN_6 ,
+  KEY_PIN_7 ,
+  KEY_PIN_8
+};
+
+char *mesgArray[NUM_KEYS] ={
+  KEY_MSG_1 ,
+  KEY_MSG_2 ,
+  KEY_MSG_3 ,
+  KEY_MSG_4 ,
+  KEY_MSG_5 ,
+  KEY_MSG_6 ,
+  KEY_MSG_7 ,
+  KEY_MSG_8
+};
+
+// Bounce buttons
+Bounce button[NUM_KEYS];
+
+void setup() {
+
+  // Initalize the button debouncing
+  for(uint8_t i = 0; i < NUM_KEYS; i++){
+    button[i] = Bounce();
+    button[i].attach(buttonArray[i], INPUT_PULLUP);
+    button[i].interval(8);
+  }
+
+  // Initialize Serial communications
+  Serial.begin(115200);
+
+}
+
+void loop() {
+
+  // Check each and every button
+  for (uint8_t i = 0; i < NUM_KEYS; i++) {
+    button[i].update();
+
+    // When button is pressed, send the corresponding message
+    if (button[i].fell()) {
+      Serial.println(mesgArray[i]);
+    }
+  }
+}
